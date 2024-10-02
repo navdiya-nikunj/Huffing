f72c023ffc9c5ceae1df3b15269461cb8b7601fd
richuak <richuakuttikattu@gmail.com>
2024-10-02 15:47:24 +0530

    change sol file to an interface

--- src/CampToken.sol
+++ src/CampToken.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract CampToken is ERC721 {
-    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
-    // TODO: Keep adding the other additional functions here
+interface IFull721 is IERC721 {
+    function mintToken() external payable;
+    function burn(uint256) external;
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+}
+
+interface CampToken is IFull721 {
+// constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
+// TODO: Keep adding the other additional functions here
 }
