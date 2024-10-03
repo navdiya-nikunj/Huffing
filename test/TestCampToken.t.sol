9fecf71ffb947ec89b3f0cbc9288dd182bba400a
richuak <richuakuttikattu@gmail.com>
2024-10-03 15:57:25 +0530

    test whitelisting

--- test/TestCampToken.t.sol
+++ test/TestCampToken.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 import {CampToken} from "../src/CampToken.sol";
 
@@ -9,7 +9,10 @@ contract TestCampTokenHuff is Test {
     string public constant NFT_NAME = "CampToken";
     string public constant NFT_SYMBOL = "CPT";
 
+    /// @dev don't change the input private keys, or the public address might change and affect tests
+    // user1: 0x233F20a935E25f6c993a71F72cCBE72c79A1F7FC, tokenId : 69
     address user1 = makeAddr("User 1");
+    // user2: 0x8993e8FD026fC11Ad77fB612fB3175C4c0C55330, tokenId: 42
     address user2 = makeAddr("User 2");
     address user3 = makeAddr("User 3");
 
@@ -21,6 +24,7 @@ contract TestCampTokenHuff is Test {
                 "CampToken"
             )
         );
+        token.whiteListMinters();
     }
 
     function testName() public view {
@@ -37,9 +41,11 @@ contract TestCampTokenHuff is Test {
         vm.prank(user1);
         token.mintToken();
         uint256 user1Balance = token.balanceOf(user1);
+        console.log("user1: ", user1);
+        console.log("user2: ", user2);
 
         assertEq(user1Balance, 1);
-        assertEq(token.ownerOf(0), user1);
+        assertEq(token.ownerOf(69), user1);
     }
 
     function testTokenIDgetsUpdated() public {
@@ -52,8 +58,10 @@ contract TestCampTokenHuff is Test {
 
         assertEq(user1Balance, 1);
         assertEq(user2Balance, 1);
-        assertEq(token.ownerOf(0), user1);
-        assertEq(token.ownerOf(1), user2);
+        // assertEq(token.ownerOf(0), user1);
+        // assertEq(token.ownerOf(1), user2);
+        assertEq(token.ownerOf(69), user1);
+        assertEq(token.ownerOf(42), user2);
     }
 
     function testDoubleMintingFromSameUserFails() public {
@@ -70,7 +78,28 @@ contract TestCampTokenHuff is Test {
     function testTransfer() public {
         vm.startPrank(user1);
         token.mintToken();
-        token.safeTransferFrom(user1, user3, 0); // (user3, 0);
-        assertEq(token.ownerOf(0), user3);
+        token.safeTransferFrom(user1, user3, 69); // (user3, 0);
+        vm.stopPrank();
+        assertEq(token.ownerOf(69), user3);
+    }
+
+    function testWhiteListing() public view {
+        // vm.prank(user3);
+        // token.whiteListMinters();
+        uint256 whiteListTokenId = token.getWhiteListedTokeIDForAddress(0xcF78399B272E71F23F00b453005e9ba0EFa9FcDc);
+        assertEq(whiteListTokenId, 8);
+    }
+
+    function testWhiteListingForTestAccount() public view {
+        // vm.prank(user3);
+        // token.whiteListMinters();
+        uint256 whiteListTokenId = token.getWhiteListedTokeIDForAddress(user1);
+        assertEq(whiteListTokenId, 69);
+    }
+
+    function testNonWhiteListedGetsReverted() public {
+        vm.prank(user3);
+        vm.expectRevert();
+        token.mintToken();
     }
 }
